48bc1a9fbb611a45ba55769ef5eff50f214c62b5
Mirco874 <mircocuof@gmail.com>
2023-10-16 23:15:12 -0700

    chore: cameras connected routing

--- main.py
+++ main.py
@@ -18,5 +18,57 @@ def loginPage():
 def registerPage():
     return render_template("register/register.html");
 
+
+
+@app.route("/surveillance/one-camera-image")
+def oneCameraImage():
+    if(len(connected_devices) != 1 ):
+        return redirect("/surveillance");
+    return "one camera image"
+
+@app.route("/surveillance/two-camera-image")
+def twoCameraImage():
+    if(len(connected_devices) != 2 ):
+        return redirect("/surveillance");
+    return "two camera image"
+
+@app.route("/surveillance/more-cameras-image")
+def moreThanTwoCameraImage():
+    if(len(connected_devices) == 0 or len(connected_devices) < 3 ):
+        return redirect("/surveillance");
+        return "more than one camera image"
+
+@app.route("/surveillance/no-cameras-added")
+def noCamerasAdded():
+    global connected_devices;
+    if(len(connected_devices) > 0 ):
+        return redirect("/surveillance");
+    return render_template("surveillance/no_camera_connected.html");
+
+@app.route("/surveillance")
+def CameraImagePage():
+    global connected_devices;
+
+    if(len(connected_devices) == 0):
+        return redirect("surveillance/no-cameras-added");
+
+    elif(len(connected_devices) == 1 ):
+        return redirect("surveillance/one-camera-image");
+
+    elif(len(connected_devices) == 2 ):
+        return redirect("surveillance/two-camera-image");
+
+    elif(len(connected_devices) > 2 ):
+        return redirect("surveillance/more-cameras-image");
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     socketIO.run(app, host = "localhost");
