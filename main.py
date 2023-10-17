bc767f2a1a69504a1d6b52d5c49e07e2b8cb7a08
Mirco874 <mircocuof@gmail.com>
2023-10-16 22:06:33 -0700

    feat: login with jwt

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify, redirect, Response;
+from flask_socketio import SocketIO, send
+
+
+app=Flask(__name__)
+app.config["SECRET"] = "secret";
+socketIO = SocketIO(app, cors_allowed_origins="*");
+
+      
+@app.route("/")
+def dashboardPage():
+    return "Dashboard page";
+
+@app.route("/auth/login")
+def loginPage():
+    return render_template("login/login.html");
+
+if __name__ == "__main__":
+    socketIO.run(app, host = "localhost");
---
+++ static/scripts/session.js
@@ -0,0 +1,32 @@
+const noLoggedUserPaths = ["/auth/login", "/auth/register"];
+const loggedUserPaths = ["/", "/surveillance/no-cameras-added", "/surveillance/one-camera-image", "/reports"]
+
+const isAuthenticated = async() => {
+    const token = localStorage.getItem("token") || "";
+    const apiUrl = "http://localhost:3000/api/v1/user/validate-token";
+
+    const requestOptions ={
+        method: "GET",
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        }
+    }
+    
+    const response = await fetch(apiUrl, requestOptions)
+    const responseJson = await response.json();
+    return (responseJson.statusCode === 200);
+}
+
+const verifySession = async() =>{
+    if(await isAuthenticated() && noLoggedUserPaths.includes(location.pathname)){
+        location.replace("/");
+    }
+
+    if(!await isAuthenticated() && loggedUserPaths.includes(location.pathname)){
+        location.replace("/auth/login?=no-authenticated");
+    }
+}
+
+
+verifySession();
