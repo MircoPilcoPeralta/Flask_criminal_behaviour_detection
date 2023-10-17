5991ae2442f2c9b246831931a3ec38f2fcc3da87
Mirco874 <mircocuof@gmail.com>
2023-10-16 22:13:40 -0700

    feat: register with jwt and redirection to dashboard

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, request, jsonify, redirect, Response;
 from flask_socketio import SocketIO, send
 
-
 app=Flask(__name__)
 app.config["SECRET"] = "secret";
 socketIO = SocketIO(app, cors_allowed_origins="*");
-
-      
+ 
 @app.route("/")
 def dashboardPage():
     return "Dashboard page";
@@ -14,6 +12,10 @@ def dashboardPage():
 @app.route("/auth/login")
 def loginPage():
     return render_template("login/login.html");
+
+@app.route("/auth/register")
+def registerPage():
+    return render_template("register/register.html");
 
 if __name__ == "__main__":
     socketIO.run(app, host = "localhost");
